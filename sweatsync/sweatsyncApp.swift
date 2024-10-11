615f387e00f3d23cbb9f96fa030f0d5c745d9d1c
aarushic <aarushichitagi@gmail.com>
2024-10-10 23:13:19 -0500

    Initial Commit

---
+++ sweatsync/sweatsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweatsyncApp.swift
+//  sweatsync
+//
+//  Created by aarushi chitagi on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct sweatsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
