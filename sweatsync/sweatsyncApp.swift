ace2dcdb87c4ba5d2b8f84f6626367867966c725
Ashwin <ashwint1200@gmail.com>
2024-10-15 18:33:53 -0500

    firebase setup

--- sweatsync/sweatsyncApp.swift
+++ sweatsync/sweatsyncApp.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct sweatsyncApp: App {
     @StateObject private var session = SessionManager()
-        
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             SplashScreen()
